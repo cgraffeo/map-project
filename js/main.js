9474f60b44d794b70c768cc2c391f9c705a9c566
Chris Graffeo <cgraffeo1@gmail.com>
2017-06-09 16:22:51 -0700

    major overhaul of files and structure

--- js/main.js
+++ js/main.js
@@ -5,18 +5,24 @@ $(document).ready(function() {
     });
 });
 
-
-
 var map;
-// Create a new blank array for all the listing markers.
+var marker;
 var markers = [];
-var locations = [
-    {title: 'Capitol', location: {lat: 44.938502, lng: -123.030474}},
-    {title: 'Salem Health - Hospital', location: {lat: 44.932337, lng: -123.034181}},
-    {title: 'Riverfront City Park', location: {lat: 44.942180, lng: -123.042549}},
-    {title: 'The Governor' +"'"+'s Cup Coffee Roasters', location: {lat: 44.940984, lng: -123.036755}},
-    {title: 'Salem Center', location: {lat: 44.943273, lng: -123.036157}}
-  ];
+var locations = [];
+var ViewModel;
+var contentString;
+
+function googleMapError(){
+  alert("Gopogle Maps API isn't loading");
+}
+
+// var locations = [
+//     {title: 'Capitol', location: {lat: 44.938502, lng: -123.030474}},
+//     {title: 'Salem Health - Hospital', location: {lat: 44.932337, lng: -123.034181}},
+//     {title: 'Riverfront City Park', location: {lat: 44.942180, lng: -123.042549}},
+//     {title: 'The Governor' +"'"+'s Cup Coffee Roasters', location: {lat: 44.940984, lng: -123.036755}},
+//     {title: 'Salem Center', location: {lat: 44.943273, lng: -123.036157}}
+//   ];
 function initMap() {
   // Constructor creates a new map
   map = new google.maps.Map(document.getElementById('map'), {
@@ -26,76 +32,100 @@ function initMap() {
   // These are the locations that will be shown to the user.
   // Normally we'd have these in a database instead.
 
-  var largeInfowindow = new google.maps.InfoWindow();
+
   var bounds = new google.maps.LatLngBounds();
+  viewModel.google(!!window.google);
   // The following group uses the location array to create an array of markers on initialize.
-  for (var i = 0; i < locations.length; i++) {
-    // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
 
-    // Push the marker to our array of markers.
-    markers.push(marker);
-    // Create an onclick event to open an infowindow at each marker.
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
+  function salemListModel(favoritePlaces){
+    $.getJSON("data.json", function(data){
+      var locationJSON = data.locations;
+      for(var i = 0; i < locationJSON.length; i++){
+        viewModel.favoritePlaces.push(new LocationModel(locationJSON[i], viewModel));
+      }
+    }).fail(function(){
+      alert("Markers not loading, Try back later");
     });
   }
-  $('#show-listings').on('click', showListings);
-  $('#hide-listings').on('click', hideListings);
-}
-// This function populates the infowindow when the marker is clicked. Info is populated based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
-    infowindow.open(map, marker);
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick',function(){
-      infowindow.setMarker = null;
-    });
-  }
-}
-// This function will loop through the markers array and display them all.
-function showListings() {
-  var bounds = new google.maps.LatLngBounds();
-  // Extend the boundaries of the map for each marker and display the marker
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-    bounds.extend(markers[i].position);
-  }
-  map.fitBounds(bounds);
-}
-// This function will loop through the listings and hide them all.
-function hideListings() {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);
-  }
-}
-// Search filter
-function ViewModel(){
+
+  var LocationModel = function(location, viewModel) {
+    var self = this;
+
+    self.title = location.name;
+    self.position = location.position;
+    self.address = location.address;
+    self.city = location.city;
+    self.state = location.state;
+    self.zipcode = location.zipcode;
+
+    self.createMarker = ko.computed(function(){
+      if (viewModel.google()){
+        self.marker = new google.maps.Marker({
+          position: self.position,
+          map: map,
+          title: self.title,
+          animation: google.maps.Animation.DROP,
+        });
+        self.contentString = ko.computed(function(){
+          return '<div><i>'+ self.title + '</i></div>' +
+          '<div>'+ self.address + '</div>' +
+          '<div>'+ self.city + ',' + self.state + ',' + self.zipcode + '</div>';
+        });
+        self.marker.addListener('click', function(){
+        // Wikipedia API
+        var wikiUrl = 'https://en.wikipedia.org/w/api.php?'+
+                      'action=opensearch&search=' + self.title +
+                      '&format=json&callback=wikiCallback';
+        $.ajax({
+          url: wikiUrl,
+          dataType: "jsonp"
+        }).done(function(response){
+          var article = response[3][0];
+          var url = '<div>'+ '<a href ="'+ article +'" target="_blank">'+ self.title +'</a></div>';
+          viewModel.largeInfoWindow.setContent(self.contentString() + url);
+          viewModel.largeInfoWindow.open(map, self.marker);
+        }).fail(function(){
+          viewModel.largeInfoWindow.setContent(self.contentString() + '<em><br>'+ "Wikipedia data isn't loading"+'</em>');
+          viewModel.largeInfoWindow.open(map, self.marker);
+        });
+        self.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout (function(){self.marker.setAnimation(null);}, 750);
+      });
+    }
+  });
+};
+
+var ViewModel = function(LocationModel) {
   var self = this;
-  this.filter = ko.observable();
-  this.locations = ko.observableArray(locations);
-  this.markers = ko.observableArray(markers);
+  self.favoritePlaces = ko.observableArray();
+  self.google = ko.observable(!!window.google);  //Sets the Google Window to False
 
-  this.visibleLocations = ko.computed(function(){
-    return this.locations().filter(function(location){
-      if(!self.filter() || location.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1)
-        return location;
-    });
-  }, this);
+  chicagoListModel(self.favoritePlaces);
+
+  self.infoWindowCreation = ko.computed(function(){
+    if (self.google()){
+      self.largeInfoWindow = new google.maps.InfoWindow();
+    }
+  });
+
+self.mapParameter = ko.observable("");
+self.visibleLocations = ko.computed(function(){
+  var filter = self.mapParameter().toLowerCase();
+  return ko.utils.arrayFilter(self.favoritePlaces(), function(location){
+    var locationMatch = location.title.toLowerCase().indexOf(searchFilter) >= 0;
+    if(location.marker){
+      location.marker.setVisible(locationMatch);
+    }
+    return locationMatch;
+  });
+});
+
+self.markerAnimator = function(location){
+  google.maps.event.trigger(location.marker, 'click');
+};
 }
+};
 
-
-ko.applyBindings( new ViewModel());
+// var largeInfowindow = new google.maps.InfoWindow();
+viewModel = new ViewModel();
+ko.applyBindings(ViewModel());
