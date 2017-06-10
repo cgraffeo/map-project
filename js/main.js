8094120bde327ce34b1ec7ca18a6d47ed7fe09e3
Chris Graffeo <cgraffeo1@gmail.com>
2017-06-10 11:09:05 -0700

    Major overhaul to project functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-(function () {
-  "use strict";
-}());
-
 $(document).ready(function() {
   $("#menu-toggle").click(function(e) {
         e.preventDefault();
@@ -20,20 +16,6 @@ function googleMapError(){
   alert("Gopogle Maps API isn't loading");
 }
 
-// var locations = [
-//     {title: 'Oregon State Capitol', location: {lat: 44.938502, lng: -123.030474}, address: '900 Court St NE', city: 'Salem', state: 'OR', zipcode: '97301'},
-//     {title: 'Salem Health - Hospital', location: {lat: 44.932337, lng: -123.034181}, address: '890 Oak St SE', city: 'Salem', state: 'OR', zipcode: '97301'},
-//     {title: 'Riverfront City Park', location: {lat: 44.942180, lng: -123.042549}, address: '200 Water St NE', city: 'Salem', state: 'OR', zipcode: '97301'},
-//     {title: 'The Governors Cup Coffee Roasters', location: {lat: 44.940984, lng: -123.036755}, address: '471 Court St NE', city: 'Salem', state: 'OR', zipcode: '97301'},
-//     {title: 'Salem Center', location: {lat: 44.943273, lng: -123.036157}, address: '401 Center St NE, Salem, OR 97301', city: 'Salem', state: 'OR', zipcode: '97301'},
-//   ];
-// var locations = [
-//     {title: 'Capitol', location: {lat: 44.938502, lng: -123.030474}},
-//     {title: 'Salem Health - Hospital', location: {lat: 44.932337, lng: -123.034181}},
-//     {title: 'Riverfront City Park', location: {lat: 44.942180, lng: -123.042549}},
-//     {title: 'The Governor' +"'"+'s Cup Coffee Roasters', location: {lat: 44.940984, lng: -123.036755}},
-//     {title: 'Salem Center', location: {lat: 44.943273, lng: -123.036157}}
-//   ];1
 function initMap(){
  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 44.938502, lng: -123.030474},
@@ -42,27 +24,31 @@ function initMap(){
 
   var bounds = new google.maps.LatLngBounds();
 
-  viewModel.google(!!window.google); //true
+  viewModel.google(!!window.google);
 
 }
 // Loading Locations Data Dynamically
-function SalemListModel(favoritePlaces){
-  $.getJSON("/js/data.json", function(data) {
-    var locationJSON = data.locations;
-    for(var i = 0; i < locationJSON.length; i++){
-      viewModel.favoritePlaces.push(new LocationModel(locationJSON[i], viewModel));
-    }
-  }).fail(function(){
-    alert("Location Data isn't loading");
-  });
+function SalemListModel(localSpots){
+  $.ajax({
+    method: 'get',
+    dataType: 'json',
+    url: '/js/data.json'
+  }).done(function(data){
+    var locations = data.locations
+    locations.forEach(function(loc, i){
+      viewModel.localSpots.push(new LocationModel(loc, viewModel));
+    })
+  }).fail(function(error){
+    console.error(error)
+  })
 }
 
 //Model - LocationModel constructor funtion
 var LocationModel = function(location, viewModel) {
   var self = this;
 
-  self.title = location.name;
-  self.position = location.position;
+  self.title = location.title;
+  self.position = location.location;
   self.address = location.address;
   self.city = location.city;
   self.state = location.state;
@@ -76,13 +62,17 @@ var LocationModel = function(location, viewModel) {
         position: self.position,
         map: map,
         title: self.title,
+        visible: true,
         animation: google.maps.Animation.DROP,
       });
+      self.marker.setVisible(true);
       //Create contentString for the InfoWindow
       self.contentString = ko.computed(function(){
-        return '<div><i>'+ self.title + '</i></div>' +
-        '<div>'+ self.address + '</div>' +
-        '<div>'+ self.city +', '+ self.state +' '+ self.zipcode + '</div>';
+        return `
+          <div><i>${self.title}</i></div>
+          <div>${self.address}</div>
+          <div>${self.city}, ${self.state} ${self.zipcode}</div>
+        `
       });
       self.marker.addListener('click', function(){
         // Wikipedia API
@@ -94,14 +84,14 @@ var LocationModel = function(location, viewModel) {
           dataType: "jsonp"
         }).done(function(response){
           var article = response[3][0];
-          var url = '<div>'+ '<a href ="'+ article +'" target="_blank">'+ self.title +'</a></div>';
+          var url = `<div><a href="${article}" target="_blank">${self.title}</a></div>`
           //Set content with InfoWindow
           viewModel.largeInfoWindow.setContent(self.contentString() + url);
           // Open LargeInfoWindow
           viewModel.largeInfoWindow.open(map, self.marker);
         }).fail(function(){
           //Set content with InfoWindow
-          viewModel.largeInfoWindow.setContent(self.contentString() + '<em><br>'+ "Wikipedia data isn't loading"+'</em>');
+          viewModel.largeInfoWindow.setContent(self.contentString() + `<em><br>Wikipedia data isn't loading</em>`);
           // Open LargeInfoWindow
           viewModel.largeInfoWindow.open(map, self.marker);
         });
@@ -109,10 +99,6 @@ var LocationModel = function(location, viewModel) {
         self.marker.setAnimation(google.maps.Animation.BOUNCE);
         setTimeout (function(){self.marker.setAnimation(null);}, 750);
       });
-      // Search box
-      // var input = document.getElementById('searchItem');
-      // var searchBox = new google.maps.places.SearchBox(input);
-      // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     }
   });
 };
@@ -120,10 +106,10 @@ var LocationModel = function(location, viewModel) {
 // ViewModel
 var ViewModel = function(LocationModel) {
   var self = this;
-  self.favoritePlaces = ko.observableArray();
+  self.localSpots = ko.observableArray();
   self.google = ko.observable(!!window.google);  //Sets the Google Window to False
 
-  SalemListModel(self.favoritePlaces);
+  SalemListModel(self.localSpots);
 
   self.infoWindowCreation = ko.computed(function(){
     if (self.google()){
@@ -133,13 +119,13 @@ var ViewModel = function(LocationModel) {
 
   // Search Filter
 
-  // mapParameter represents the user input into the search box
-  self.mapParameter = ko.observable("");
+  // userQuery represents the user input into the search box
+  self.userQuery = ko.observable("");
 
   // Search List is a filtered list of the
   self.visibleLocations = ko.computed(function(){
-    var searchFilter = self.mapParameter().toLowerCase();
-    return ko.utils.arrayFilter(self.favoritePlaces(), function(location){
+    var searchFilter = self.userQuery().toLowerCase();
+    return ko.utils.arrayFilter(self.localSpots(), function(location){
       var locationMatch = location.title.toLowerCase().indexOf(searchFilter) >= 0;
       if(location.marker){
         location.marker.setVisible(locationMatch);
